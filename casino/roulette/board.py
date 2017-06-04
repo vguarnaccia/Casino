a4343e2816b9e9f31c91b84bbced3ec9809a9343
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-06-04 16:34:07 -0700

    dropped casefold again

--- casino/roulette/board.py
+++ casino/roulette/board.py
@@ -101,8 +101,7 @@ class Wheel(object):
         Return:
             set: all outcomes matching ``name``.
         """
-        return {oc for oc in self.all_outcomes if name.casefold()
-                in oc.name.casefold()}
+        return {oc for oc in self.all_outcomes if name.lower() in oc.name.lower()}
 
     def __next__(self):
         """Select bin from bins
