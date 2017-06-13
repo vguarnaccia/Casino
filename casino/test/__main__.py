1fa16a250e32239571cdfb613e041bd701fbe4a9
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-06-12 21:30:19 -0700

    integrate unittests with doctests

--- casino/test/__main__.py
+++ casino/test/__main__.py
@@ -1,5 +1,14 @@
+import doctest
 import unittest
 
-from .test_roulette import *
+from .. import roulette
+from . import test_roulette
 
-unittest.main()
+suite = unittest.TestSuite()
+
+# Mix unittests and doctests into the same suite
+suite.addTest(doctest.DocTestSuite(roulette))
+suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(test_roulette))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(suite)
